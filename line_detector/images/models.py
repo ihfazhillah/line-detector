43f3bb3312d1fefc108581160965701b475c0404
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-10-17 05:39:22 +0700

    update model

--- line_detector/images/models.py
+++ line_detector/images/models.py
@@ -11,6 +11,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to="lines/")
     title = models.CharField(max_length=255, null=True, blank=True)
     options = JSONField()
+    scale = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
